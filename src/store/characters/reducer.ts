21b331866b9fa112bc45c0d4f9494ea68a60b28d
MurilloCunha <jcunhamurillo@gmail.com>
2021-11-20 17:20:18 -0300

    feat: add character reducer

---
+++ src/store/characters/reducer.ts
@@ -0,0 +1,68 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import RickAndMortyService from "../../services/rick-and-morty-Service";
+
+interface Location {
+  name: string,
+  url: string
+}
+
+export interface Character {
+  id: number,
+  name: string,
+  status: string,
+  species: string,
+  type: string,
+  gender: string,
+  origin: Location,
+  location: Location,
+  image: string,
+  episode: string[],
+  url: string,
+  created: string
+}
+
+export interface Characters {
+  list: Character[],
+  status: 'idle' | 'loading' | 'failed'
+}
+const initialState: Characters = {
+  list: [],
+  status:'idle'
+}
+
+//criar characters service
+export const fetchCharacters = createAsyncThunk(
+  'characters/fetchCharacters',
+  async(): Promise<Character[]> => {
+    const response = await RickAndMortyService.getCharacters
+    return response
+  }
+)
+
+
+export const charactersSlice = createSlice({
+  name: 'characters',
+  initialState,
+  reducers:{
+    addCharacter: (state,action: PayloadAction<Character>) => {
+      state.list.push(action.payload)
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCharacters.pending, state => {
+      state.status = 'loading'
+      })
+      .addCase(fetchCharacters.fulfilled, (state, action) => {
+        state.status = 'idle'
+        state.list = action.payload
+      })
+      .addCase(fetchCharacters.rejected, state => {
+        state.status = 'failed'
+      })
+  }
+})
+
+export const { addCharacter } = charactersSlice.actions
+
+export default charactersSlice.reducer;
