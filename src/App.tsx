8cb9ed7b866171f23d81b6a2d02aae816172507a
Mraimundo <fmdumundo18@gmail.com>
2022-04-09 16:00:50 -0300

    feat: add eslint end prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-const App: React.FC = () => {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
-};
+import React from 'react';
+
+const App: React.FC = () => (
+  <div>
+    <h1>Hello, World!</h1>
+  </div>
+);
 
 export default App;
